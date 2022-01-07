666d851101333b75afcad0423ab650b079ef07ff
Chelsea Zheng <51794314+czheng181920@users.noreply.github.com>
2022-01-07 15:23:35 -0500

    commit

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 'use strict';
 
 //use JavaScript to handle events 
-//use document.querySelector('.btn') to get the button reference
-const switcher = document.querySelector('.theme-btn');
+//use document.querySelector('.theme-btn') to get the button reference
+const switcher = document.querySelector('.inside-btn');
 switcher.addEventListener('click', function(){
     document.body.classList.toggle('dark-theme')  //changes theme
 
     //update label for button
     var className = document.body.className;
     if(className == "light-theme"){ 
-        document.getElementById('btn-theme-inside-text').textContent = "Dark"; 
+        this.textContent = "Dark"; 
     } else{
-        document.getElementById('btn-theme-inside-text').textContent = "Light";     }
+        this.textContent = "Light";     }
 
     console.log('current class name: ' + className); //console message
 
