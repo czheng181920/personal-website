cabe3193eab2ccc3d8477697c96a0e80430316dd
Chelsea Zheng <51794314+czheng181920@users.noreply.github.com>
2022-01-09 00:57:23 -0500

    commit

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ switcher.addEventListener('click', function(){
     console.log('current class name: ' + className); //console message
 
 });
+
+function readMoreFunction(){
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "less";
+        moreText.style.display = "inline";
+  }
+}
